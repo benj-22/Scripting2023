d9b1aa6be7978d23d4e8f591c9feb758a316b31a
benj-22 <143044902+benj-22@users.noreply.github.com>
2023-09-05 15:21:58 -0600

    added character "Jimmy" to SillyGame and changed file name

--- SillyGame.cs
+++ SillyGame.cs
@@ -4,25 +4,33 @@ public class Program
 {
 	public Weapon miniGun;
 	public PowerUp weaponPowerUp;
+	public Characters jimmy;
 	
 	public void Main()
 	{
 		miniGun = new Weapon();
 		weaponPowerUp = new PowerUp();
+		jimmy = new Characters();
+		
+		Console.WriteLine(jimmy.mainCharacterName + " is you!");
 		
 		miniGun.ammoCount += 30;
 		miniGun.firePower += weaponPowerUp.powerLevel;
 		Console.WriteLine(miniGun.weaponName + ": ammo count = " + miniGun.ammoCount);
-		Console.WriteLine("Minigun's firepower is now " + miniGun.firePower + ": POWER UP!!!");
+		Console.WriteLine("Minigun's firepower is now " + miniGun.firePower + ": POWER UP!!! That's Broken!");
 	}
 }
 
 public class Weapon {
 	public int ammoCount = 10;
 	public string weaponName = "Mini Gun";
-	public float firePower = 11.5f;
+	public float firePower = 11.4f;
 }
 
 public class PowerUp {
-	public float powerLevel = 2.7f;
+	public float powerLevel = 35e3f;
+}
+	
+public class Characters {
+	public string mainCharacterName = "Jimmy";
 }
