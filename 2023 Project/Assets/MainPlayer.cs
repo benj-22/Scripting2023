0c54c46e3240dc9a317e94195fb336c666a7c59e
Benj Rawlings <10830671@uvu.edu>
2023-09-27 16:20:31 -0600

    add

--- 2023 Project/Assets/MainPlayer.cs
+++ 2023 Project/Assets/MainPlayer.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Example4 : MonoBehaviour
+public class MainPlayer : MonoBehaviour
 {
+    public string myName = "none";
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("I am alive, and my name is " + myName);
     }
 }
