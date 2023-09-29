ea42035fc86ca6ee287c77a4a08205c3d64184b9
Benj Rawlings <10830671@uvu.edu>
2023-09-28 23:15:19 -0600

    worked on more tutorials

---
+++ 2023 Project/Assets/ActiveObjects.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveObjects : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+}
