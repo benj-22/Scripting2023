1f278de664e47b3cbc4862fe72409dcc8a9dd8be
Benj Rawlings <10830671@uvu.edu>
2023-09-28 23:59:53 -0600

    axis

---
+++ 2023 Project/Assets/AxisExample.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+/*Input.GetAxis works like GetButton and Key, but returns float
+    instead of bool between -1 and 1
+    set up in input manager */
+public class AxisExample : MonoBehaviour
+{
+    public float range;
+    public Text textOutput;
+    
+    
+    void Update () 
+    {
+        float h = Input.GetAxis("Horizontal");
+        float xPos = h * range;
+        
+        transform.position = new Vector3(xPos, 2f, 0);
+        textOutput.text = "Value Returned: " + h.ToString("F2");  
+    }
+}+
