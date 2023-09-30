3e350d2f1593b599e6faf35e3093d576d9148808
Benj Rawlings <10830671@uvu.edu>
2023-09-29 22:36:19 -0600

    commit

---
+++ TheWonderfulCube/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float moveSpeed = 5f;
+    private float jumpForce = 5f;
+    private float gravity = 9.81f;
+
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    private bool isJumping;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float horizontalInput Input.GetAxis("Horizontal");
+        float verticalInput Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        moveDirection = transform.TransformDirection(moveDirection);
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
+}
