79ab735eb36d0719f229c4225bf31b70f98fcb26
Benj Rawlings <10830671@uvu.edu>
2023-10-16 14:35:07 -0600

    first tut done

--- UFO Defense Force/Assets/Scripts/MainMenu.cs
+++ UFO Defense Force/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public int sceneToLoad;
+
     // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(sceneToLoad); //Which scene will load
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
+        Debug.Log("Quit Game");
     }
 }
