afcdded8d44979b21b400674e0abea140593cf82
Benj Rawlings <10830671@uvu.edu>
2023-10-24 00:08:22 -0600

    finished lab

---
+++ Unit 5/Lab/Unit5Project/Assets/IntData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Int Data", menuName = "Int Data")]
+public class IntData : ScriptableObject
+{
+    [SerializeField] private int value;
+    public int Value
+    {
+        get {return value;}
+        set {this.value = value;}
+    }
+}
