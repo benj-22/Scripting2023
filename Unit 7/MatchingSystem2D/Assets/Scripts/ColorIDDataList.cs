94db8ed7f333e75e33b0cbf44aceb2e7c409cdf5
Benj Rawlings <10830671@uvu.edu>
2023-11-10 17:58:55 -0700

    Finished Lab 7b

--- Unit 7/MatchingSystem2D/Assets/Scripts/ColorIDDataList.cs
+++ Unit 7/MatchingSystem2D/Assets/Scripts/ColorIDDataList.cs
@@ -6,4 +6,14 @@ using UnityEngine;
 public class ColorIDDataList : ScriptableObject
 {
     public List<ColorID> colorIDList;
+
+    public ColorID currentColor;
+
+    private int num;
+
+    public void SetCurrentColorRandomly()
+    {
+        num = num = Random.Range(0, colorIDList.Count);
+        currentColor = colorIDList[num];
+    }
 }
--- Unit 7/MatchingSystem2D/Assets/Scripts/SpriteBehavior.cs
+++ Unit 7/MatchingSystem2D/Assets/Scripts/SpriteBehavior.cs
@@ -15,4 +15,9 @@ public class SpriteBehavior : MonoBehaviour
     {
         rendererObj.color = obj.value;
     }
+
+    public void ChangeRendererColor(ColorIDDataList obj)
+    {
+        rendererObj.color = obj.currentColor.value;
+    }
 }
