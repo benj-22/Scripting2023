898d785516163fae9abda154bb444bdaf020f7ff
Benj Rawlings <10830671@uvu.edu>
2023-11-10 17:12:18 -0700

    lab

--- Unit 7/MatchingSystem2D/Assets/Scripts/Instancer.cs
+++ Unit 7/MatchingSystem2D/Assets/Scripts/Instancer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Instancer : ScriptableObject
 {
     public GameObject prefab;
+    private int num;
     public void CreateInstance()
     {
         Instantiate(prefab);
@@ -15,4 +16,28 @@ public class Instancer : ScriptableObject
         Instantiate(prefab, obj.value, Quaternion.identity);
     }
 
+    public void CreateInstanceFromList(Vector3DataList obj)
+    {
+        for(var i = 0; i <obj.vector3List.Count; i++)
+        {
+            Instantiate(prefab, obj.vector3List[i].value, Quaternion.identity);
+        }
+    }
+
+    public void CreateInstanceFromListCounting(Vector3DataList obj)
+    {
+       Instantiate(prefab, obj.vector3List[num].value, Quaternion.identity);
+       num++;
+       if(num == obj.vector3List.Count)
+       {
+            num = 0;
+       }
+    }
+
+    public void CreateInstanceListRandomly(Vector3DataList obj)
+    {
+        num = Random.Range(0, obj.vector3List.Count);
+        Instantiate(prefab, obj.vector3List[num].value, Quaternion.identity);
+    }
+    
 }
--- Unit 7/MatchingSystem2D/Assets/Scripts/Vector3DataList.cs
+++ Unit 7/MatchingSystem2D/Assets/Scripts/Vector3DataList.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
-public class Vector3DataList : MonoBehaviour
+[CreateAssetMenu]
+
+public class Vector3DataList : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List <Vector3Data> vector3List;
 }
